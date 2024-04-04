9bea8a4e59c73c243f410774ce9a130b3cdde800
Alexander Blaschko-Schänzer <alexander.blaschko@gmail.com>
2024-04-04 08:07:10 +0200

    Initial commit of my StockTradingSimulator

---
+++ src/main/java/StockTradingSimulator/StockTrader.java
@@ -0,0 +1,50 @@
+package StockTradingSimulator;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class StockTrader {
+    private double balance;
+    private List<Stock> portfolio;
+
+    public StockTrader(double initialBalance) {
+        this.balance = initialBalance;
+        this.portfolio = new ArrayList<>();
+    }
+
+    public void buyStock(Stock stock, int quantity) {
+        double cost = stock.getPrice() * quantity;
+        if (cost <= balance) {
+            balance -= cost;
+            for (int i = 0; i < quantity; i++) {
+                portfolio.add(stock);
+            }
+            System.out.println("Bought " + quantity + " shares of " + stock.getSymbol());
+        } else {
+            System.out.println("Insufficient funds to buy " + quantity + " shares of " + stock.getSymbol());
+        }
+    }
+
+    public void sellStock(Stock stock, int quantity) {
+        int count = 0;
+        for (int i = 0; i < portfolio.size(); i++) {
+            if (portfolio.get(i).getSymbol().equals(stock.getSymbol())) {
+                portfolio.remove(i);
+                balance += stock.getPrice();
+                count++;
+                if (count == quantity)
+                    break;
+            }
+        }
+        System.out.println("Sold " + quantity + " shares of " + stock.getSymbol());
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Stock> getPortfolio() {
+        return portfolio;
+    }
+}
---
+++ src/main/java/StockTradingSimulator/StockTradingSimulation.java
@@ -0,0 +1,23 @@
+package StockTradingSimulator;
+
+public class StockTradingSimulation {
+    public static void main(String[] args) {
+        Stock apple = new Stock("AAPL", 150.50);
+        Stock microsoft = new Stock("MSFT", 300.75);
+
+        // Example dataset
+        StockTrader trader = new StockTrader(10000);
+        trader.buyStock(apple, 5);
+        trader.buyStock(microsoft, 3);
+
+        // Selling some stocks
+        trader.sellStock(apple, 2);
+
+        // Print portfolio and balance
+        System.out.println("Portfolio:");
+        for (Stock stock : trader.getPortfolio()) {
+            System.out.println(stock.getSymbol());
+        }
+        System.out.println("Balance: $" + trader.getBalance());
+    }
+}
